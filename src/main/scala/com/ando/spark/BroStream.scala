e749edd548b45cc2e8fc096c1c7e32fc85e0d602
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-11 23:33:20 +0700

    bismillah

--- src/main/scala/com/ando/spark/BroStream.scala
+++ src/main/scala/com/ando/spark/BroStream.scala
@@ -74,8 +74,7 @@ object BroStream extends StreamUtils {
           r.getAs[String](1),
           r.getAs[String](2),
           r.getAs[String](3),
-          r.getAs[String](4),
-          r.getAs[String](5)
+          r.getAs[String](4)
         ))
 
 
