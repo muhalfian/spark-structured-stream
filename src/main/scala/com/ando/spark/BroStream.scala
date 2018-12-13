6d8b97a60c9cd77fc3cea80071f64299cf176fda
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-13 18:23:08 +0700

    bismillah

--- src/main/scala/com/ando/spark/BroStream.scala
+++ src/main/scala/com/ando/spark/BroStream.scala
@@ -64,7 +64,7 @@ object BroStream extends StreamUtils {
       val connDf = parsedLogData
         .map((r:Row) => ConnCountObj(
           r.getAs[String](0),
-          r.getAs[String](1),
+          r.getAs[Array(String)](1),
           r.getAs[String](2),
           r.getAs[String](3),
           r.getAs[String](4)
