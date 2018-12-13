ef8d4b1e9512b27e9ead5e0d85f7f4c75d7ff748
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-13 21:46:43 +0700

    bismillah

--- src/main/scala/com/ando/spark/BroStream.scala
+++ src/main/scala/com/ando/spark/BroStream.scala
@@ -58,7 +58,7 @@ object BroStream extends StreamUtils {
          .as("col")
       )
       .select(from_json(col("col"), schema)
-       .getField("link, authors, publish_date, title, text")
+       .getField("*")
       .alias("conn")
       )
 
