ee4d444312b13bc9672db8d3d4590b7b59a17e1d
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-14 16:08:12 +0700

    bismillah

--- src/main/scala/com/muhalfian/spark/BroStream.scala
+++ src/main/scala/com/muhalfian/spark/BroStream.scala
@@ -75,7 +75,7 @@ object BroStream extends StreamUtils {
 
       println(parsedLogData)
       println(connDf)
-      println(connDf.text)
+      println(connDf)
 
     //   //Sink to Mongodb
     //   val ConnCountQuery = connDf
