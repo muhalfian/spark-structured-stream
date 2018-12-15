32f6e5fef1775fd1d50fd46896a476d50d887b5a
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-15 23:31:14 +0700

    bismillah

--- src/main/scala/com/muhalfian/spark/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/MediaStream.scala
@@ -78,7 +78,7 @@ object MediaStream extends StreamUtils {
             .awaitTermination()
     }
 
-    def preprocess(text: String): String = {
+    def preprocess(text: Column): Column = {
         regexp_replace(text, "\\s+", "")
     }
 }
