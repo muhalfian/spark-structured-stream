6cd585daeb0a1beabcb7dba0a0195af98075cb85
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-27 14:37:52 +0700

    aggregation : bismillah

--- src/main/scala/com/muhalfian/spark/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/MediaStream.scala
@@ -262,8 +262,8 @@ object MediaStream extends StreamUtils {
 
         var masterWords = new Array[String](52000)
         val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2, "d" -> 3, "e" -> 4, "f" -> 5, "g" -> 6, "h" -> 7, "i" -> 8, "j" -> 9, "k" -> 10, "l" -> 11, "m" -> 12, "n" -> 13, "o" -> 14, "p" -> 15, "q" -> 16, "r" -> 17, "s" -> 18, "t" -> 19, "u" -> 20, "v" -> 21, "w" -> 22, "x" -> 23, "y" -> 24, "z" -> 25)
-        // var masterDataAgg = Seq.empty[(Int, Int, Int)].toDF("link_id", "word_id", "counts")
-        var masterDataAgg = Seq((0,0,0)).toDF("link_id", "word_id", "count")
+        var masterDataAgg = Seq.empty[(Int, Int, Int)].toDF("link_id", "word_id", "counts")
+        // var masterDataAgg = Seq((0,0,0)).toDF("link_id", "word_id", "count")
         println(masterDataAgg.printSchema())
 
         // val schemaAgg = StructType(
@@ -309,7 +309,7 @@ object MediaStream extends StreamUtils {
                 }
 
                 println(id, currentPoint, count)
-                var temp = Seq(id, currentPoint, count).toDF("link_id", "word_id", "count")
+                var temp = Seq((id, currentPoint, count)).toDF("link_id", "word_id", "count")
                 println(temp.printSchema())
                 // var temp = List(List(id, currentPoint, count)).map(x =>(x(0), x(1), x(2))).toDF
                 // var tempDF = sqlContext.createDataFrame(spark.sparkContext.parallelize(temp), schemaAgg)
@@ -318,8 +318,9 @@ object MediaStream extends StreamUtils {
             }
 
             println(masterWords)
-            val result = Seq(content, id)
-            result
+            // val result = Seq(content, id)
+            // result
+            content
         })
 
         // Aggregate Running in DF
