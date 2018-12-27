e3309987137a7ecf24ed189bdad82754aa9e3ee3
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-27 12:46:15 +0700

    aggregation : debugging

--- src/main/scala/com/muhalfian/spark/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/MediaStream.scala
@@ -263,7 +263,7 @@ object MediaStream extends StreamUtils {
         var masterWords = new Array[String](52000)
         val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2, "d" -> 3, "e" -> 4, "f" -> 5, "g" -> 6, "h" -> 7, "i" -> 8, "j" -> 9, "k" -> 10, "l" -> 11, "m" -> 12, "n" -> 13, "o" -> 14, "p" -> 15, "q" -> 16, "r" -> 17, "s" -> 18, "t" -> 19, "u" -> 20, "v" -> 21, "w" -> 22, "x" -> 23, "y" -> 24, "z" -> 25)
         // var masterDataAgg = Seq.empty[(Int, Int, Int)].toDF("link_id", "word_id", "counts")
-        var masterDataAgg = Seq(1, 20000, 1).toDF("link_id", "word_id", "counts")
+        var masterDataAgg = Seq.empty[(Int, Int, Int)].toDF("link_id", "word_id", "counts")
         var currentPoint = 0
 
         // Aggregate User Defined FunctionmonotonicallyIncreasingId
@@ -298,9 +298,9 @@ object MediaStream extends StreamUtils {
                     currentPoint = index
                 }
 
-                var temp = Seq(id, currentPoint, count)
+                var temp = Seq(id, currentPoint, count).toDF("link_id", "word_id", "counts")
                 println(temp)
-                masterDataAgg = masterDataAgg.union(temp.toDF())
+                masterDataAgg = masterDataAgg.union(temp)
             }
 
             println(masterWords)
