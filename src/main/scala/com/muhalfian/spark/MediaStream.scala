95a33df41ee8bc47364029aca2fac7a7a8b73b71
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-27 16:06:13 +0700

    aggregation : update bismillah

--- src/main/scala/com/muhalfian/spark/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/MediaStream.scala
@@ -320,7 +320,8 @@ object MediaStream extends StreamUtils {
                 }
 
                 println(currentPoint, count)
-                var temp = List(List(currentPoint, count)).map(x =>(x(0), x(1))).toDF
+                // var temp = List(List(currentPoint, count)).map(x =>(x(0), x(1))).toDF
+                var temp = List(List(currentPoint, count)).map(x =>(x(0), x(1)))
                 var tempDF = spark.createDataFrame(spark.sparkContext.parallelize(temp), schemaAgg)
                 // println(temp)
                 // var temp = Seq((currentPoint, count)).toDF()
