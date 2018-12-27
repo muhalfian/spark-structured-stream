c3202889c1dbf2166e801272509217e2ed22e62b
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-27 08:29:52 +0700

    aggregation : debugging

--- src/main/scala/com/muhalfian/spark/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/MediaStream.scala
@@ -269,13 +269,13 @@ object MediaStream extends StreamUtils {
             //
             // println(counted)
 
-            // println(content)
+            println(content)
 
             for ((token,count) <- counted) {
                 // printf("key: %s, value: %s\n", token, count)
                 // print(token)
                 var char:String = token.take(1)
-                // println(token + " -> " + char)
+                println(token + " -> " + char)
                 var startPoint = indexWords(char)
                 var endPoint = startPoint + 999
 
