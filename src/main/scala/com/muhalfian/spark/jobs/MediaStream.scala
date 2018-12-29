2160d1f6a6b962863141496fdb3eb0a9809b2e40
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-29 12:34:35 +0700

    aggregation : fixing aggregation

--- src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
@@ -61,101 +61,26 @@ object MediaStream extends StreamUtils {
     val filteredDF = TextTools.remover.transform(regexDF)
 
     val preprocessDF = filteredDF.select("link", "source", "authors", "image", "publish_date", "title", "text", "text_preprocess")
-                              .withColumn("text_preprocess", TextTools.stemming(col("text_preprocess").cast("string")))
+                              .withColumn("text_preprocess", TextTools.saggregationtemming(col("text_preprocess").cast("string")))
 
     // ======================== AGGREGATION ================================
 
-
-    val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
-                         "d" -> 3, "e" -> 4, "f" -> 5,
-                         "g" -> 6, "h" -> 7, "i" -> 8,
-                         "j" -> 9, "k" -> 10, "l" -> 11,
-                         "m" -> 12, "n" -> 13, "o" -> 14,
-                         "p" -> 15, "q" -> 16, "r" -> 17,
-                         "s" -> 18, "t" -> 19, "u" -> 20,
-                         "v" -> 21, "w" -> 22, "x" -> 23,
-                         "y" -> 24, "z" -> 25)
-
-    var currentPoint = 0
-
-    preprocessDF.map(row => {
-      val row1 = row.getAs[String]("text_preprocess")
-      // val make = if (row1.toLowerCase == "125") "S" else "its 123"
-      println("map text preprocess")
-      (row(0),row1,row(0)) }).collect().foreach(println)
-
-
-    val wordDict = udf((content: String, link: String) => {
-      val splits = content.split(" ")
-        .toSeq
-        .map(_.trim)
-        .filter(_ != "")
-
-      val counted = splits.groupBy(identity).mapValues(_.size)
-
-      for ((token,count) <- counted) {
-        var char = token.take(1)
-        // println(token + " -> " + char)
-
-        var point = indexWords(char)
-
-        var currentPoint = masterWords(point).indexWhere(_ == token)
-        if(currentPoint == -1){
-          masterWords(point) += token
-          masterWordsIndex += token
-        }
-        // println(link, currentPoint, count)
-        // masterListAgg += ((link, splits.toArray))
-      }
-      println("Building Dictionary : " + masterWordsIndex.size)
-      content
-    })
-
-    // Aggregate Running in DF
     val aggregateDF = preprocessDF
-      .withColumn("text_preprocess", wordDict(col("text_preprocess").cast("string"), col("link").cast("string")))
+      .withColumn("text_preprocess", AggTools.aggregate(col("text_preprocess").cast("string")))
 
-    val aggregation = udf((content: String) => {
+    // ============================ CLUSTERING =================================
 
-      val splits = content.split(" ").toSeq.map(_.trim).filter(_ != "")
-
-      val intersectCounts: Map[String, Int] =
-        masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
-      val wordCount: Array[Int] = masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).toArray
-
-      // println(wordCount.mkString(" "))
-      println("Aggregate array : " + wordCount.size)
-      masterAgg = masterAgg :+ wordCount
-
+    val clustering = udf((content: String) => {
       content
     })
 
-    val aggregateDF2 = aggregateDF
-        .withColumn("text_preprocess", aggregation(col("text_preprocess").cast("string")))
-
-    // masterWordsIndex.clear
-    // for(row <- masterWords){
-    //   masterWordsIndex = masterWordsIndex ++ row
-    // }
-
-    // // extract data from masterList
-    // var groupMasterList = masterListAgg.groupBy(_._1)
-    //
-    // masterAgg = Vector[Array[Int]]()
-    // for((group, splits) <- groupMasterList){
-    //   val intersectCounts: Map[String, Int] =
-    //     masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
-    //   val wordCount: Array[Int] = masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).toArray
-    //
-    //   println(wordCount.mkString(" "))
-    //   println("Size : " + wordCount.size)
-    //   masterAgg = masterAgg :+ wordCount
-    // }
+    val clusterDF = aggregateDF
+        .withColumn("text_preprocess", clustering(col("text_preprocess").cast("string")))
 
     // =========================== SINK ====================================
 
     //Show Data after processed
-    aggregateDF2.writeStream
+    clusterDF.writeStream
       .format("console")
       // .option("truncate","false")
       .start()
--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -1,15 +1,42 @@
 package com.muhalfian.spark.util
 
 object AggTools {
-  // val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
-  //                      "d" -> 3, "e" -> 4, "f" -> 5,
-  //                      "g" -> 6, "h" -> 7, "i" -> 8,
-  //                      "j" -> 9, "k" -> 10, "l" -> 11,
-  //                      "m" -> 12, "n" -> 13, "o" -> 14,
-  //                      "p" -> 15, "q" -> 16, "r" -> 17,
-  //                      "s" -> 18, "t" -> 19, "u" -> 20,
-  //                      "v" -> 21, "w" -> 22, "x" -> 23,
-  //                      "y" -> 24, "z" -> 25)
-  //
-  // var masterListAgg = MutableList[(String, Int, Int)]()
+  val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
+                       "d" -> 3, "e" -> 4, "f" -> 5,
+                       "g" -> 6, "h" -> 7, "i" -> 8,
+                       "j" -> 9, "k" -> 10, "l" -> 11,
+                       "m" -> 12, "n" -> 13, "o" -> 14,
+                       "p" -> 15, "q" -> 16, "r" -> 17,
+                       "s" -> 18, "t" -> 19, "u" -> 20,
+                       "v" -> 21, "w" -> 22, "x" -> 23,
+                       "y" -> 24, "z" -> 25)
+
+
+  val aggregate = udf((content: String) => {
+    val splits = content.split(" ").toSeq.map(_.trim).filter(_ != "")
+
+    val grouped = splits.groupBy(identity)
+
+    for ((token,tokenArr) <- grouped) {
+      var point = indexWords(token.take(1))
+
+      var currentPoint = masterWords(point).indexWhere(_ == token)
+      if(currentPoint == -1){
+        masterWords(point) += token
+        masterWordsIndex += token
+      }
+      // println(link, currentPoint, count)
+      // masterListAgg += ((link, splits.toArray))
+    }
+
+    val intersectCounts: Map[String, Int] =
+      masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
+    val wordCount: Array[Int] = masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).toArray
+
+    // println(wordCount.mkString(" "))
+    println("Aggregate array : " + wordCount.size)
+    masterAgg = masterAgg :+ wordCount
+
+    content
+  })
 }
