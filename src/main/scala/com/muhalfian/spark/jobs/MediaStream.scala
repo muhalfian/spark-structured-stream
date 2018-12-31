3431fc9f3059316618ed15c21317445589648675
Muhammad Alfian <muhammadalfian500@gmail.com>
2018-12-31 15:41:18 +0700

    aggregation : debugging

--- src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
@@ -16,36 +16,11 @@ import org.apache.spark.sql.functions.{explode, split, col, lit, concat, udf, fr
 
 import org.apache.spark.ml.linalg._
 
-import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.ml.feature.LabeledPoint
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-
 // import org.apache.lucene.analysis.id.IndonesianAnalyzer
 // import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 
 
 object MediaStream extends StreamUtils {
-
-  val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
-                       "d" -> 3, "e" -> 4, "f" -> 5,
-                       "g" -> 6, "h" -> 7, "i" -> 8,
-                       "j" -> 9, "k" -> 10, "l" -> 11,
-                       "m" -> 12, "n" -> 13, "o" -> 14,
-                       "p" -> 15, "q" -> 16, "r" -> 17,
-                       "s" -> 18, "t" -> 19, "u" -> 20,
-                       "v" -> 21, "w" -> 22, "x" -> 23,
-                       "y" -> 24, "z" -> 25)
-
-  var countWords = 0
-  var masterLink = ArrayBuffer[String]()
-  val masterWords = ArrayBuffer.fill(26,1)(("",0))
-  var masterWordsIndex = ArrayBuffer[String]()
-  var masterWordsCount = ArrayBuffer[(String, Seq[(Int, Double)])]()
-  var temp : Seq[LabeledPoint] = Seq(LabeledPoint(0, Vectors.sparse(1, Seq((0, 0.0)))))
-  var masterAgg : Dataset[LabeledPoint] = temp.toDS()
-  // var masterAgg = ArrayBuffer[Vector]()
-  // var masterListAgg = ArrayBuffer[(String, Int, Int)]()
-
 
   def main(args: Array[String]): Unit = {
 
@@ -85,71 +60,13 @@ object MediaStream extends StreamUtils {
 
     // ======================== AGGREGATION ================================
 
-
-    val aggregate = udf((content: Seq[String], link: String) => {
-      // val splits = content.split(" ").toSeq.map(_.trim).filter(_ != "")
-
-      val grouped = content.groupBy(identity).mapValues(_.size)
-      var tempSeq = Seq[(Int, Double)]()
-
-      for ((token,count) <- grouped) {
-        var point = indexWords(token.take(1))
-
-        var index = masterWords(point).indexWhere(_._1 == token)
-        var currentPoint = 0
-        if(index == -1){
-          masterWordsIndex += token
-          currentPoint = masterWordsIndex.size - 1
-          masterWords(point) += ((token, currentPoint))
-
-        } else {
-          currentPoint = masterWords(point)(index)._2
-        }
-
-        tempSeq = tempSeq :+ (currentPoint, count.toDouble)
-
-        // // println(link, currentPoint, count)
-        // val intersectCounts: Map[String, Int] =
-        //     masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
-        // val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
-
-        // println(wordCount.mkString(" "))
-        // println("Aggregate array : " + wordCount.size)
-        // masterAgg = masterAgg :+ wordCount
-      }
-
-      masterLink += link
-
-      countWords = masterWordsIndex.size
-
-      var temp: Seq[LabeledPoint] = Seq(LabeledPoint(masterLink.size-1, Vectors.sparse(countWords, tempSeq)))
-      var dataset: Dataset[LabeledPoint] = temp.toDS()
-
-      masterAgg = masterAgg.union(dataset)
-
-
-      println("aggregate " + masterWordsIndex.size)
-
-      // masterListAgg += (splits.to[ArrayBuffer])
-
-      // masterAgg.clear
-      // for(row <- masterListAgg){
-      //   val intersectCounts: Map[String, Int] =
-      //     masterWordsIndex.intersect(row).map(s => s -> row.count(_ == s)).toMap
-      //   val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
-      //
-      //   // println(wordCount.mkString(" "))
-      //   println("Aggregate array : " + wordCount.size)
-      //   masterAgg += wordCount
-      // }
-      content
-    })
-
     // val aggregateDF = preprocessDF
     //   .withColumn("text_preprocess", AggTools.aggregate(col("text_preprocess").cast("string")))
 
+
+
     val aggregateDF = preprocessDF
-      .withColumn("text_preprocess", AggTools.aggregate(col("text_preprocess"), col("link").cast("string")))
+      .withColumn("text_preprocess", AggTools.aggregate(col("text_preprocess"), col("link").cast("string"), spark))
 
     // ============================ CLUSTERING =================================
 
--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -5,88 +5,90 @@ import org.apache.spark.sql.functions.{split, col, udf}
 import scala.collection.mutable.ArrayBuffer
 
 // import org.apache.spark.ml.linalg._
-
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.feature.LabeledPoint
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object AggTools {
-  // val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
-  //                      "d" -> 3, "e" -> 4, "f" -> 5,
-  //                      "g" -> 6, "h" -> 7, "i" -> 8,
-  //                      "j" -> 9, "k" -> 10, "l" -> 11,
-  //                      "m" -> 12, "n" -> 13, "o" -> 14,
-  //                      "p" -> 15, "q" -> 16, "r" -> 17,
-  //                      "s" -> 18, "t" -> 19, "u" -> 20,
-  //                      "v" -> 21, "w" -> 22, "x" -> 23,
-  //                      "y" -> 24, "z" -> 25)
-  //
-  // var countWords = 0
-  // var masterLink = ArrayBuffer[String]()
-  // val masterWords = ArrayBuffer.fill(26,1)(("",0))
-  // var masterWordsIndex = ArrayBuffer[String]()
-  // var masterWordsCount = ArrayBuffer[(String, Seq[(Int, Double)])]()
-  // var temp : Seq[LabeledPoint] = Seq(LabeledPoint(0, Vectors.sparse(1, Seq((0, 0.0)))))
-  // var masterAgg : Dataset[LabeledPoint] = temp.toDS
-  // // var masterAgg = ArrayBuffer[Vector]()
-  // // var masterListAgg = ArrayBuffer[(String, Int, Int)]()
-  //
-  // val aggregate = udf((content: Seq[String], link: String) => {
-  //   // val splits = content.split(" ").toSeq.map(_.trim).filter(_ != "")
-  //
-  //   val grouped = content.groupBy(identity).mapValues(_.size)
-  //   var tempSeq = Seq[(Int, Double)]()
-  //
-  //   for ((token,count) <- grouped) {
-  //     var point = indexWords(token.take(1))
-  //
-  //     var index = masterWords(point).indexWhere(_._1 == token)
-  //     var currentPoint = 0
-  //     if(index == -1){
-  //       masterWordsIndex += token
-  //       currentPoint = masterWordsIndex.size - 1
-  //       masterWords(point) += ((token, currentPoint))
-  //
-  //     } else {
-  //       currentPoint = masterWords(point)(index)._2
-  //     }
-  //
-  //     tempSeq = tempSeq :+ (currentPoint, count.toDouble)
-  //
-  //     // // println(link, currentPoint, count)
-  //     // val intersectCounts: Map[String, Int] =
-  //     //     masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
-  //     // val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
-  //
-  //     // println(wordCount.mkString(" "))
-  //     // println("Aggregate array : " + wordCount.size)
-  //     // masterAgg = masterAgg :+ wordCount
-  //   }
-  //
-  //   masterLink += link
-  //
-  //   countWords = masterWordsIndex.size
-  //
-  //   var temp: Seq[LabeledPoint] = Seq(LabeledPoint(masterLink.size-1, Vectors.sparse(countWords, tempSeq)))
-  //   var dataset: Dataset[LabeledPoint] = temp.toDS
-  //
-  //   masterAgg = masterAgg.union(dataset)
-  //
-  //
-  //   println("aggregate " + masterWordsIndex.size)
-  //
-  //   // masterListAgg += (splits.to[ArrayBuffer])
-  //
-  //   // masterAgg.clear
-  //   // for(row <- masterListAgg){
-  //   //   val intersectCounts: Map[String, Int] =
-  //   //     masterWordsIndex.intersect(row).map(s => s -> row.count(_ == s)).toMap
-  //   //   val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
-  //   //
-  //   //   // println(wordCount.mkString(" "))
-  //   //   println("Aggregate array : " + wordCount.size)
-  //   //   masterAgg += wordCount
-  //   // }
-  //   content
-  // })
+  val indexWords = Map("a" -> 0, "b" -> 1, "c" -> 2,
+                       "d" -> 3, "e" -> 4, "f" -> 5,
+                       "g" -> 6, "h" -> 7, "i" -> 8,
+                       "j" -> 9, "k" -> 10, "l" -> 11,
+                       "m" -> 12, "n" -> 13, "o" -> 14,
+                       "p" -> 15, "q" -> 16, "r" -> 17,
+                       "s" -> 18, "t" -> 19, "u" -> 20,
+                       "v" -> 21, "w" -> 22, "x" -> 23,
+                       "y" -> 24, "z" -> 25)
+
+  var countWords = 0
+  var masterLink = ArrayBuffer[String]()
+  val masterWords = ArrayBuffer.fill(26,1)(("",0))
+  var masterWordsIndex = ArrayBuffer[String]()
+  var masterWordsCount = ArrayBuffer[(String, Seq[(Int, Double)])]()
+  var temp : Seq[LabeledPoint] = Seq(LabeledPoint(0, Vectors.sparse(1, Seq((0, 0.0)))))
+  var masterAgg : Dataset[LabeledPoint] = temp.toDS
+  // var masterAgg = ArrayBuffer[Vector]()
+  // var masterListAgg = ArrayBuffer[(String, Int, Int)]()
+
+  val aggregate = udf((content: Seq[String], link: String, spark : SparkSession) => {
+    // val splits = content.split(" ").toSeq.map(_.trim).filter(_ != "")
+
+    import spark.implicits._
+
+    val grouped = content.groupBy(identity).mapValues(_.size)
+    var tempSeq = Seq[(Int, Double)]()
+
+    for ((token,count) <- grouped) {
+      var point = indexWords(token.take(1))
+
+      var index = masterWords(point).indexWhere(_._1 == token)
+      var currentPoint = 0
+      if(index == -1){
+        masterWordsIndex += token
+        currentPoint = masterWordsIndex.size - 1
+        masterWords(point) += ((token, currentPoint))
+
+      } else {
+        currentPoint = masterWords(point)(index)._2
+      }
+
+      tempSeq = tempSeq :+ (currentPoint, count.toDouble)
+
+      // // println(link, currentPoint, count)
+      // val intersectCounts: Map[String, Int] =
+      //     masterWordsIndex.intersect(splits).map(s => s -> splits.count(_ == s)).toMap
+      // val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
+
+      // println(wordCount.mkString(" "))
+      // println("Aggregate array : " + wordCount.size)
+      // masterAgg = masterAgg :+ wordCount
+    }
+
+    masterLink += link
+
+    countWords = masterWordsIndex.size
+
+    var temp: Seq[LabeledPoint] = Seq(LabeledPoint(masterLink.size-1, Vectors.sparse(countWords, tempSeq)))
+    var dataset: Dataset[LabeledPoint] = temp.toDS
+
+    masterAgg = masterAgg.union(dataset)
+
+
+    println("aggregate " + masterWordsIndex.size)
+
+    // masterListAgg += (splits.to[ArrayBuffer])
+
+    // masterAgg.clear
+    // for(row <- masterListAgg){
+    //   val intersectCounts: Map[String, Int] =
+    //     masterWordsIndex.intersect(row).map(s => s -> row.count(_ == s)).toMap
+    //   val wordCount = Vectors.dense(masterWordsIndex.map(intersectCounts.getOrElse(_, 0)).map(_.toDouble).toArray)
+    //
+    //   // println(wordCount.mkString(" "))
+    //   println("Aggregate array : " + wordCount.size)
+    //   masterAgg += wordCount
+    // }
+    content
+  })
 }
