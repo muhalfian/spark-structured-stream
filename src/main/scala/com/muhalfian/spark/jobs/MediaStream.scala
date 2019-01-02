4097d927f6cb4dd5d7b843529ca6c7387b1fb577
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-01-02 23:13:28 +0700

    aggregation : stringify

--- src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
@@ -76,13 +76,12 @@ object MediaStream extends StreamUtils {
     val aggregateDF = preprocessDF
       .withColumn("text_aggregate", AggTools.aggregate(col("text_preprocess"), col("link").cast("string")))
 
-    // val stringify = udf((vs: Seq[String]) => vs match {
-    //   case null => null
-    //   case _    => s"""[${vs.mkString(",")}]"""
-    // })
-    //
-    // val customDF = aggregateDF
-    //   .withColumn("text_aggregate", stringify(col("text_aggregate")))
+    val stringify = udf((vs: String) => {
+      vs.replaceAll("[\\]\\[]", "")
+    })
+
+    val customDF = aggregateDF
+      .withColumn("text_aggregate", stringify(col("text_aggregate").cast("string")))
 
     // ============================ CLUSTERING =================================
 
@@ -115,14 +114,14 @@ object MediaStream extends StreamUtils {
     // =========================== SINK ====================================
 
     //Show Data after processed
-    val printConsole = aggregateDF.writeStream
+    val printConsole = customDF.writeStream
       .format("console")
       // .option("truncate","false")
       .start()
 
 
 
-    val aggregateSave = aggregateDF.writeStream
+    val aggregateSave = customDF.writeStream
       .option("checkpointLocation", "hdfs://blade1-node:9000/checkpoint/online_media/aggregation")
       .option("path","hdfs://blade1-node:9000/online_media/aggregation")
       .outputMode("append")
