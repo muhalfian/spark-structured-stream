a0a223180cb6be9ca3fa2f31170e2d3804ed73ce
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-01-03 17:13:27 +0700

    aggregation : bismillah

--- src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MediaStream.scala
@@ -80,7 +80,7 @@ object MediaStream extends StreamUtils {
     val customDF = aggregateDF
       .withColumn("text_aggregate", TextTools.stringify(col("text_aggregate").cast("string")))
       .withColumn("text_preprocess", TextTools.stringify(col("text_preprocess").cast("string")))
-      .withColumn("text", TextTools.stringify(col("text_preprocess").cast("string")))
+      .withColumn("text", TextTools.stringify(col("text").cast("string")))
 
     // ============================ CLUSTERING =================================
 
