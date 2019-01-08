eb4ff0db3f2718a6025b55225b82a6904613f71e
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-01-08 14:56:42 +0700

    aggregation : debugging

--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -48,8 +48,8 @@ object AggTools extends StreamUtils {
     }).toSeq
 
     // filter under max value / 2
-    var threshold = tempSeq.maxBy(_._2)
-    tempSeq = tempSeq.filter(_._2 > threshold / 2)
+    var threshold = tempSeq.maxBy(_._2)._2 / 2
+    tempSeq = tempSeq.filter(_._2 > threshold)
 
     // for ((token,count) <- grouped) {
     //   // var point = indexWords(token.take(1))
