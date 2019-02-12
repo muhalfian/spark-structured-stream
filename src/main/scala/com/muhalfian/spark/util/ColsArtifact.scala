711491fed04698dfc0de7817454d0f6657de623b
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-12 23:19:24 +0700

    general : update schema database

--- src/main/scala/com/muhalfian/spark/util/ColsArtifact.scala
+++ src/main/scala/com/muhalfian/spark/util/ColsArtifact.scala
@@ -6,7 +6,7 @@ object ColsArtifact {
   val rawSchema : StructType = StructType(Seq(
     StructField("link", StringType,true),
     StructField("source", StringType, true),
-    StructField("authors", StringType, true),
+    StructField("description", StringType, true),
     StructField("image", StringType, true),
     StructField("publish_date", StringType, true),
     StructField("title", StringType, true),
@@ -17,7 +17,7 @@ object ColsArtifact {
   case class masterData(
     link: String,
     source: String,
-    authors: String,
+    description: String,
     image: String,
     publish_date: String,
     title: String,
