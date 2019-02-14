b8fd98a31c9530e1e81017aad1da82263d6e0dfe
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-14 10:13:40 +0700

    general : debugging new file

---
+++ src/main/scala/com/muhalfian/spark/jobs/Initialize.scala
@@ -0,0 +1,134 @@
+package com.muhalfian.spark.jobs
+
+import com.muhalfian.spark.util._
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql._
+
+import scala.collection.mutable.{MutableList, ArrayBuffer, Set, HashSet}
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{explode, split, col, lit, concat, udf, from_json}
+
+import org.apache.spark.ml.linalg._
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+import org.apache.spark.sql.streaming.Trigger
+
+// import org.apache.spark.ml.clustering.BisectingKMeans
+// import com.muhalfian.spark.ml.BisectingKMeans
+
+
+object MediaStream extends StreamUtils {
+
+  def main(args: Array[String]): Unit = {
+
+    // ===================== LOAD SPARK SESSION ============================
+
+    val spark = getSparkSession(args)
+    import spark.implicits._
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // ======================== READ STREAM ================================
+
+    // read data stream from Kafka
+    val kafka = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", PropertiesLoader.kafkaBrokerUrl)
+      .option("subscribe", PropertiesLoader.kafkaTopic)
+      .option("startingOffsets", """ {"online_media" : { "0" : 0 } } """)
+      .option("endingOffsets", """ {"online_media" : { "0" : 100 } } """)
+      .load()
+
+    // Transform data stream to Dataframe
+    val kafkaDF = kafka.selectExpr("CAST(value AS STRING)").as[(String)]
+      .select(from_json($"value", ColsArtifact.rawSchema).as("data"))
+      .select("data.*")
+      .withColumn("raw_text", concat(col("title"), lit(" "), col("text"))) // add column aggregate title and text
+
+
+    // =================== PREPROCESS SSparkSessionASTRAWI =============================
+
+    val regexDF = TextTools.regexTokenizer.transform(kafkaDF)
+
+    val filteredDF = TextTools.remover.transform(regexDF)
+
+    val preprocessDF = filteredDF
+                        .withColumn("text_preprocess", TextTools.stemming(col("text_preprocess")))
+
+    val selectedDF = preprocessDF.select("link", "source", "description", "image", "publish_date", "title", "text", "text_preprocess")
+                        .withColumn("text_selected", TextTools.select(col("text_preprocess")))
+
+    // ======================== AGGREGATION ================================
+
+    val aggregateDF = selectedDF
+      .withColumn("text_aggregate", AggTools.aggregate(col("text_selected"), col("link")))
+
+    val customDF = aggregateDF
+      // .withColumn("text_aggregate", TextTools.stringify(col("text_aggregate").cast("string")))
+      .withColumn("text_preprocess", TextTools.stringify(col("text_preprocess").cast("string")))
+      .withColumn("text_selected", TextTools.stringify(col("text_selected").cast("string")))
+      .withColumn("text", TextTools.stringify(col("text").cast("string")))
+
+    // ============================ CLUSTERING =================================
+
+    // val clustering = udf((content: String) => {
+    //   // var masterAggUpdate = ArrayBuffer[Vector]()
+    //   // val dimension = AggTools.masterWordsIndex.size
+    //   //
+    //   // for(row <- AggTools.masterAgg){
+    //   //   var vecZeros = Vectors.zeros(dimension)
+    //   //   var key = 0
+    //   //   while(key < row.size){
+    //   //     vecZeros.toArray(key) = row(key)
+    //   //     key += 1
+    //   //   }
+    //   //   masterAggUpdate += vecZeros
+    //   // }
+    //
+    //   // print("Dimension array - 0 : " + masterAggUpdate(0).size)
+    //   content
+    // })
+    //
+    // val clusterDF = aggregateDF
+    //     .withColumn("text_aggregate", clustering(col("text_aggregate").cast("string")))
+
+    // val kmeans = new BisectingKMeans().setK(3).setFeaturesCol("text_aggregate").setPredictionCol("prediction")
+    // val model = kmeans.fit(aggregateDF)
+    // val clusterDF = model.transform(aggregateDF)
+    // // println(predicted.show)
+
+    // =========================== SINK ====================================
+
+
+    //Show Data after processed
+    val printConsole = customDF.writeStream
+      .format("console")
+      // .option("truncate","false")
+      .start()
+
+    // val automaticClustering = customDF
+    //   .map(r => RowArtifact.clusterData(r))
+    //   .writeStream
+    //   .format("console")
+    //   .outputMode("append")
+    //   .trigger(Trigger.Once())
+    //   .foreach(WriteUtil.clustering)
+    //   .start()
+
+    val saveMasterData = customDF
+          .map(r => RowArtifact.rowMasterData(r))
+          .writeStream
+          .outputMode("append")
+          .foreach(WriterUtil.masterData)
+          .start()
+
+    printConsole.awaitTermination()
+    saveMasterData.awaitTermination()
+  }
+
+}
--- src/main/scala/com/muhalfian/spark/util/WriterUtils.scala
+++ src/main/scala/com/muhalfian/spark/util/WriterUtils.scala
@@ -47,42 +47,4 @@ object WriterUtil {
       true
     }
   }
-
-  val clustering = new ForeachWriter[ColsArtifact.masterData] {
-    var masterCollection : String = PropertiesLoader.mongoUrl + "prayuga.master_data"
-    val writeConfig: WriteConfig = WriteConfig(Map("uri" -> masterCollection))
-    var mongoConnector: MongoConnector = _
-    var masterDataCounts: ArrayBuffer[ColsArtifact.masterData] = _
-
-    override def process(value: ColsArtifact.masterData): Unit = {
-      masterDataCounts.append(value)
-    }
-
-    override def close(errorOrNull: Throwable): Unit = {
-      if (masterDataCounts.nonEmpty) {
-        mongoConnector.withCollectionDo(writeConfig, { collection: MongoCollection[Document] =>
-          collection.insertMany(masterDataCounts.map(sc => {
-            var doc = new Document()
-            doc.put("link", sc.link)
-            doc.put("source", sc.source)
-            doc.put("description", sc.description)
-            doc.put("image", sc.image)
-            doc.put("publish_date", sc.publish_date)
-            doc.put("title", sc.title)
-            doc.put("text", sc.text)
-            doc.put("text_preprocess", sc.text_preprocess)
-            doc.put("text_aggregate", sc.text_aggregate)
-            doc.put("text_selected", sc.text_selected)
-            doc
-          }).asJava)
-        })
-      }
-    }
-
-    override def open(partitionId: Long, version: Long): Boolean = {
-      mongoConnector = MongoConnector(writeConfig.asOptions)
-      masterDataCounts = new ArrayBuffer[ColsArtifact.masterData]()
-      true
-    }
-  }
 }
