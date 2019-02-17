9f0b4cc5f21cb2644ead2978cc63e3cc9d950dde
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-17 22:53:59 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/Dictionary.scala
+++ src/main/scala/com/muhalfian/spark/jobs/Dictionary.scala
@@ -71,7 +71,6 @@ object Dictionary extends StreamUtils {
     val selectedDF = preprocessDF.select("link", "source", "description", "image", "publish_date", "title", "text", "text_preprocess")
                         .withColumn("text_selected", TextTools.select(col("text_preprocess")))
 
-    println(masterWord.count.toInt)
     masterWord.show()
 
     // ======================== SAVE DICTIONARY ================================
@@ -90,6 +89,7 @@ object Dictionary extends StreamUtils {
                       .collect.toList(0)
         } catch {
            case unknown => {
+             println(masterWord.count.toInt)
              index = masterWord.count.toInt
            }
         }
