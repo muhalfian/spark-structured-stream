1130dc29d9aff3deafce312644cdffb02eccf51d
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-17 21:55:24 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/Dictionary.scala
+++ src/main/scala/com/muhalfian/spark/jobs/Dictionary.scala
@@ -88,7 +88,7 @@ object Dictionary extends StreamUtils {
                       .rdd.map(r => r.getInt(1))
                       .collect.toList(0)
         } catch {
-           case _ : NullPointerException => {
+           case _ : java.lang.NullPointerException => {
              index = masterWord.count.toInt
            }
         }
