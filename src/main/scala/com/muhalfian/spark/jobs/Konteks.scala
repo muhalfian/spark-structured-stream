3e7252351e10ce6003d37bb5115d079e1880f112
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-20 06:32:08 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/Konteks.scala
+++ src/main/scala/com/muhalfian/spark/jobs/Konteks.scala
@@ -45,13 +45,13 @@ object Konteks {
     val sqlContext = sparkSession.sqlContext
     import sparkSession.implicits._
 
-    // val gid = APP_NAME + Instant.now.getEpochSecond
+    val gid = APP_NAME + Instant.now.getEpochSecond
 
     val kafkaParams = scala.collection.immutable.Map[String, Object](
       "bootstrap.servers" -> kafkaBrokers,
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
-      // "group.id" -> gid,
+      "group.id" -> gid,
       "auto.offset.reset" -> "earliest",
       "enable.auto.commit" -> (false: java.lang.Boolean)
     ).asJava
