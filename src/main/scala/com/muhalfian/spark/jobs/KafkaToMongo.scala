96070c68cfd91982f2c05a5e1f4c074d40408325
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-23 15:52:51 +0700

    general : add conversion kafka to mongo

---
+++ src/main/scala/com/muhalfian/spark/jobs/KafkaToMongo.scala
@@ -0,0 +1,58 @@
+package com.muhalfian.spark.jobs
+
+import com.muhalfian.spark.util._
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql._
+
+import scala.collection.mutable.{MutableList, ArrayBuffer, Set, HashSet}
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{explode, split, col, lit, concat, udf, from_json}
+
+import org.apache.spark.ml.linalg._
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+import org.apache.spark.sql.streaming.Trigger
+
+// import org.apache.spark.ml.clustering.BisectingKMeans
+// import com.muhalfian.spark.ml.BisectingKMeans
+
+
+object MediaStream extends StreamUtils {
+
+  def main(args: Array[String]): Unit = {
+
+    // ===================== LOAD SPARK SESSION ============================
+
+    val spark = getSparkSession(args)
+    import spark.implicits._
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // ======================== READ STREAM ================================
+
+    // read data stream from Kafka
+    val kafka = spark
+      .read
+      .format("kafka")
+      .option("kafka.bootstrap.servers", PropertiesLoader.kafkaBrokerUrl)
+      .option("subscribePattern", "online_media.*")
+      .option("startingOffsets", """{"online_media":{"0":0}}""")
+      .option("endingOffsets", """{"online_media":{"0":10}}""")
+      .load()
+
+    // Transform data stream to Dataframe
+    val kafkaDF = kafka.selectExpr("CAST(value AS STRING)").as[(String)]
+      .select(from_json($"value", ColsArtifact.rawSchema).as("data"))
+      .select("data.*")
+      .withColumn("raw_text", concat(col("title"), lit(" "), col("text"))) // add column aggregate title and text
+
+    // =========================== SINK ====================================
+
+    MongoSpark.save(kafkaDF.write.option("collection", "data_init").mode("overwrite"))
+  }
+
+}
