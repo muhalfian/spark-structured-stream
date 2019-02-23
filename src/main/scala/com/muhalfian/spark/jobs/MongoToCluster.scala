8c559e077f70c19754494f3d19eca9ed5974c66f
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-23 19:52:12 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/MongoToCluster.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MongoToCluster.scala
@@ -49,10 +49,10 @@ object MongoToCluster extends StreamUtils {
 
     val aggregateRDD = mongoRDD.map(r => {
       // var tempSeq = r.getAs[WrappedArray[String]](9).map( row => {
-      var tempSeq = r.get("text_selected", new java.util.ArrayList[(String, Double)]())
+      var tempSeq = r.get("text_selected", new java.util.ArrayList[String]())
 
-      tempSeq.map( word => {
-        // var word = row.drop(1).dropRight(1).split("\\,")
+      tempSeq.map( row => {
+        var word = row.drop(1).dropRight(1).split("\\,")
         var index = AggTools.masterWordsIndex.indexWhere(_ == word(0))
         if(index == -1){
           AggTools.masterWordsIndex += word(0)
