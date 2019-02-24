bc292e2f5a0eb51c7a6e9d9dcf0de11586060ad4
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-24 15:31:50 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/MongoToCluster.scala
+++ src/main/scala/com/muhalfian/spark/jobs/MongoToCluster.scala
@@ -128,7 +128,7 @@ object MongoToCluster extends StreamUtils {
 
     // ======================== WRITE MONGO ================================
 
-    val writeConfig = WriteConfig(Map("uri" -> "mongodb://10.252.37.112/prayuga", "database" -> "prayuga", "collection" -> "data_init", "replaceDocument" -> true))
+    val writeConfig = WriteConfig(Map("uri" -> "mongodb://10.252.37.112/prayuga", "database" -> "prayuga", "collection" -> "data_init", "replaceDocument" -> true), Some(ReadConfig(sc)))
     MongoSpark.save(dataArray, writeConfig)
 
 
