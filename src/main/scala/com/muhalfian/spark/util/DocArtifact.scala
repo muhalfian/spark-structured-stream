010c747b891e285a95ff3959077b80ed116780d6
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-24 14:22:34 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/DocArtifact.scala
+++ src/main/scala/com/muhalfian/spark/util/DocArtifact.scala
@@ -6,15 +6,15 @@ object DocArtifact {
 
   def dataInit(r:Document) = {
     ColsArtifact.dataInit(
-      r.getAs[String](0),
-      r.getAs[String](1),
-      r.getAs[String](2),
-      r.getAs[String](3),
-      r.getAs[String](4),
-      r.getAs[String](5),
-      r.getAs[String](6),
-      r.getAs[Array](7),
-      r.getAs[Array](8)
+      r.get("link", String),
+      r.get("source", String),
+      r.get("description", String),
+      r.get("image", String),
+      r.get("publish_date", String),
+      r.get("title", String),
+      r.get("text", String),
+      r.get("text_preprocess", new java.util.ArrayList[String]()),
+      r.get("text_selected", new java.util.ArrayList[String]())
     )
   }
 }
