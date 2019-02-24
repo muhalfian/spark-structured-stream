8a36b6de5395145c4ecbc9b588bff7cd80046bb9
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-24 22:51:29 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/KafkaToMongo.scala
+++ src/main/scala/com/muhalfian/spark/jobs/KafkaToMongo.scala
@@ -77,6 +77,6 @@ object KafkaToMongo extends StreamUtils {
     MongoSpark.write(selectedDF).mode("append").option("uri","mongodb://10.252.37.112/prayuga").option("collection","data_init").save();
     WriterUtil.saveBatchMongo("master_word",masterWord)
 
-    println("jumlah master_word : " + AggTools.masterWordsIndex.size())
+    println("jumlah master_word : " + AggTools.masterWordsIndex.size)
   }
 }
