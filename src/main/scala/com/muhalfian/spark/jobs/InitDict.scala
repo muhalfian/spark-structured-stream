9ca682e7ec5148b8226fffae7ad71dac09f35882
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-24 23:35:35 +0700

    general : debugging

---
+++ src/main/scala/com/muhalfian/spark/jobs/InitDict.scala
@@ -0,0 +1,60 @@
+package com.muhalfian.spark.jobs
+
+import com.muhalfian.spark.util._
+
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.sql._
+
+import scala.collection.mutable.{MutableList, ArrayBuffer, Set, HashSet, WrappedArray}
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.{explode, split, col, lit, concat, udf, from_json}
+
+import org.apache.spark.ml.linalg._
+
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+
+import org.apache.spark.sql.streaming.Trigger
+
+import scala.collection.JavaConversions._
+import org.bson.Document
+
+import com.muhalfian.spark.util._
+import com.mongodb.spark.config._
+import com.mongodb.spark.MongoSpark
+
+// import org.apache.spark.ml.clustering.BisectingKMeans
+// import com.muhalfian.spark.ml.BisectingKMeans
+
+
+object InitDict extends StreamUtils {
+
+  def main(args: Array[String]): Unit = {
+
+    // ===================== LOAD SPARK SESSION ============================
+
+    val spark = getSparkSession(args)
+    import spark.implicits._
+    spark.sparkContext.setLogLevel("ERROR")
+    val sc = spark.sparkContext
+
+    // ======================== READ MONGO ================================
+
+    val readConfig = ReadConfig(Map("uri" -> "mongodb://10.252.37.112/prayuga", "database" -> "prayuga", "collection" -> "data_init"), Some(ReadConfig(sc)))
+    val mongoRDD = MongoSpark.load(sc, readConfig)
+
+    // ======================== AGGREGATION ================================
+
+    val dict = AggTools.initDictionary(mongoRDD)
+
+    val masterWord = sc.parallelize(AggTools.masterWordAgg())
+
+    // ======================== WRITE MONGO ================================
+
+    WriterUtil.saveBatchMongo("master_word",masterWord)
+
+  }
+
+}
