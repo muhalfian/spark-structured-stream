e93dfa9b4a1d2886e257bb487f6411a18232e99a
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-02-26 07:01:35 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/KafkaToMongo.scala
+++ src/main/scala/com/muhalfian/spark/jobs/KafkaToMongo.scala
@@ -45,7 +45,7 @@ object KafkaToMongo extends StreamUtils {
       .option("kafka.bootstrap.servers", PropertiesLoader.kafkaBrokerUrl)
       .option("subscribePattern", "online_media.*")
       .option("startingOffsets", """{"online_media":{"0":0}}""")
-      .option("endingOffsets", """{"online_media":{"0":1900}}""")
+      .option("endingOffsets", """{"online_media":{"0":2000}}""")
       .load()
 
     // Transform data stream to Dataframe
