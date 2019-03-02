a5944651cacf73b3b0067367519f7cc8000044f7
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-03 05:17:28 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -10,6 +10,7 @@ import com.muhalfian.spark.jobs.OnlineStream
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.ml.feature.LabeledPoint
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.types._
 
 import org.apache.spark.rdd.RDD
 import org.bson.Document
