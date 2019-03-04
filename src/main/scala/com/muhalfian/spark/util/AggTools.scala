ebd43b9f09dd586f6d0703c76a3f4fedd0825ace
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-04 21:37:16 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -28,7 +28,7 @@ object AggTools {
   import spark.implicits._
 
   var masterWordsIndex = ArrayBuffer[String]()
-  var masterWordCount =
+  // var masterWordCount = 
 
   // read master word
   val readConfig = ReadConfig(Map("uri" -> "mongodb://10.252.37.112/prayuga", "database" -> "prayuga", "collection" -> "master_word_2"))
