326a1f7f45636bd1e7751cca5dc94e9fcef60f6f
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-11 10:46:14 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -91,7 +91,7 @@ object AggTools {
       var indexStat = masterWord.indexWhere(_._1 == word(0))
       if(indexStat == -1){
         println("add to database : " + word(0))
-        masterWord :+ Array(WrappedArray(word(0), masterWord.size-1))
+        masterWord :+ Array((word(0), masterWord.size-1))
         index = masterWord.size - 1
       } else {
         index = masterWord(indexStat)._2
