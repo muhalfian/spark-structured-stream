abe4e60893139831e026d04a42dcc744deea88c5
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-13 06:59:45 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/AggTools.scala
+++ src/main/scala/com/muhalfian/spark/util/AggTools.scala
@@ -125,13 +125,7 @@ object AggTools {
 
       var tempSeq = tempJava.map( row => {
         var word = row.drop(1).dropRight(1).split("\\,")
-        var index = masterWordsIndex.indexWhere(_ == word(0))
-        if(index == -1){
-          masterWordsIndex += word(0)
-          index = masterWordsIndex.size - 1
-        }
-
-        (index, word(1).toDouble)
+        (word(0), word(1))
       }).toSeq
 
       val vectorData = Vectors.sparse(size, tempSeq.sortWith(_._1 < _._1)).toDense.toArray
