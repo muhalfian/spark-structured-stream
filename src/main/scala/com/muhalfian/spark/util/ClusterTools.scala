ed1d09b5e4e729badad9c0dd4eb2a98a4ec114c3
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-14 06:35:06 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
@@ -58,7 +58,7 @@ object ClusterTools {
     }).toSeq
     // var cent = centTupple.drop(1).dropRight(1).split("\\,")
     var centVec = Vectors.sparse(size, cent.sortWith(_._1 < _._1)).toDense.toArray
-    var zeroVec = Vectors.sparse(size, Seq((1,0.0))).toDense.toArray
+    var zeroVec = Array.fill(size)(0.0)
     println(zeroVec)
     var dist = 1 - CosineSimilarity.cosineSimilarity(centVec, zeroVec)
     (data._2, dist)
