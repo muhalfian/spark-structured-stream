687288fe635c60be2a40db9ce37b662b3912bf91
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-13 17:17:51 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
@@ -129,7 +129,7 @@ object ClusterTools {
     val newData = Vectors.sparse(size, tempSeq.sortWith(_._1 < _._1)).toDense.toArray
 
     val distData = centroidArr.map(data => {
-      var centTupple = data._1.toSeq
+      var centTupple = data._1
       var cent = centTupple.drop(1).dropRight(1).split("\\,")
       var centVec = Vectors.sparse(size, cent.sortWith(_._1 < _._1)).toDense.toArray
       var dist = 1 - CosineSimilarity.cosineSimilarity(centVec, newData)
