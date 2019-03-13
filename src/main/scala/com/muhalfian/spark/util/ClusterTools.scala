a3f4ce36036bb70e94845975dc1290867b312dfe
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-13 15:50:40 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
@@ -44,7 +44,7 @@ object ClusterTools {
   // var centroidArr = MongoSpark.load(spark, readConfig).collect
   // centroidArr.foreach(println)
   var centroidArr = MongoSpark.load(spark, readConfig).map(row => {
-    (row.getAs[String]("_id"),row.getAs[Integer]("centroid"),row.getAs[Double]("cluster"),row.getAs[Integer]("n"),row.getAs[Array[String]]("radius"))
+    (row.getAs[org.bson.Document]("_id"),row.getAs[Integer]("centroid"),row.getAs[Double]("cluster"),row.getAs[Integer]("n"),row.getAs[Array[String]]("radius"))
   }).collect
   centroidArr.foreach(println)
   // masterWord = ArrayBuffer(words: _*)
