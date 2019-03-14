b262b7d7630b005e896721219de55cfdd0b3d8d7
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-14 09:30:52 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
@@ -128,7 +128,7 @@ object ClusterTools {
     val masterCluster = cluster.map( index => {
       val start = """[""""
       val end = """"]"""
-      val cent = centroid(index.toInt).zipWithIndex.map( row => (row._2, row._1)).filter(_._2 > 0.0).map(_.toString).mkString(start, "\",\"", end
+      val cent = centroid(index.toInt).zipWithIndex.map( row => (row._2, row._1)).filter(_._2 > 0.0).map(_.toString).mkString(start, "\",\"", end)
       val r = radius(index.toInt)
       val i = index.toInt
       val size = n(index.toInt)
