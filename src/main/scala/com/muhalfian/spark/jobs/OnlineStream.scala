55c09b88b3c04a68e15eee869afccc2ca586d96f
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-20 12:11:41 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/OnlineStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/OnlineStream.scala
@@ -28,21 +28,13 @@ import com.mongodb.spark.config._
 
 object OnlineStream extends StreamUtils {
 
+  // ===================== LOAD SPARK SESSION ============================
+
   val spark = getSparkSessionPlain()
   import spark.implicits._
   spark.sparkContext.setLogLevel("ERROR")
 
-  // val masterWord = MongoSpark.load(OnlineStream.spark)
-  // masterWord.show()
-  // var masterWordCount = masterWord.count.toInt
-
   def main(args: Array[String]): Unit = {
-
-    // ===================== LOAD SPARK SESSION ============================
-
-    // val spark = getSparkSession(args)
-    // import spark.implicits._
-    // spark.sparkContext.setLogLevel("ERROR")
 
     // ======================== READ STREAM ================================
 
@@ -65,7 +57,7 @@ object OnlineStream extends StreamUtils {
       .select("data.*")
       .withColumn("raw_text", concat(col("title"), lit(" "), col("text"))) // add column aggregate title and text
 
-    // =================== PREPROCESS SSparkSessionASTRAWI =============================
+    // =================== PREPROCESS SASTRAWI =============================
 
     val regexDF = TextTools.regexTokenizer.transform(kafkaDF)
 
@@ -81,8 +73,9 @@ object OnlineStream extends StreamUtils {
     val selectedDF = mergeDF.select("link", "source", "description", "image", "publish_date", "title", "text", "text_preprocess")
                         .withColumn("text_selected", TextTools.select(col("text_preprocess")))
 
-    // // ======================== AGGREGATION ================================
-    //
+
+    // ======================== AGGREGATION ================================
+
     val aggregateDF = selectedDF
       .withColumn("text_aggregate", AggTools.aggregateMongo(col("text_selected")))
 
@@ -96,50 +89,8 @@ object OnlineStream extends StreamUtils {
       .withColumn("text_selected", TextTools.stringify(col("text_selected").cast("string")))
       .withColumn("text", TextTools.stringify(col("text").cast("string")))
 
-    //
-    // val aggregateDF = selectedDF.map( d => {
-    //   d.getAs[WrappedArray[String]](8).map( row => {
-    //     masterWord.show()
-    //     var word = row.drop(1).dropRight(1).split("\\,")
-    //     var index = masterWord.filter($"word" === word(0)).rdd.map(r => r.getInt(1)).collect.toList(0)
-    //
-    //     // var index = Try(
-    //     //
-    //     //         ).getOrElse(
-    //     //           masterWordCount
-    //     //         )
-    //
-    //     if(index == masterWordCount){
-    //       masterWordCount += 1
-    //     } else {
-    //       index = 0
-    //       word = null
-    //     }
-    //
-    //     val kata = word(0)
-    //     var query = s"{index: $index, word: '$kata'}"
-    //     println(s"doc save to mongodb : {index: $index, word: '$kata'}")
-    //     // Document.parse(query)
-    //     row
-    //   })
-    // })
-
-    //
-    // val customDF = aggregateDF
-    //   .withColumn("text_aggregate", TextTools.stringify(col("text_aggregate").cast("string")))
-    //   .withColumn("text_preprocess", TextTools.stringify(col("text_preprocess").cast("string")))
-    //   .withColumn("text_selected", TextTools.stringify(col("text_selected").cast("string")))
-    //   .withColumn("text", TextTools.stringify(col("text").cast("string")))
-
-    // ====================== ONLINE CLUSTERING ================================
-
-
 
     // =========================== SINK ====================================
-
-
-
-
 
     val saveMasterData = customDF
           .map(r => RowArtifact.rowMasterDataUpdate(r))
