89cc48c1f42453ebebe899e0ee7a41c837e053c4
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-20 14:45:04 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ReadUtils.scala
+++ src/main/scala/com/muhalfian/spark/util/ReadUtils.scala
@@ -2,11 +2,13 @@ package com.muhalfian.spark.util
 
 import com.muhalfian.spark.jobs.OnlineStream
 
-import org.bson.Document
+// import org.bson.Document
 import org.apache.spark.rdd.RDD
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config._
 import com.mongodb.spark.rdd.MongoRDD
+
+import org.mongodb.scala.Document
 
 import org.apache.spark.sql._
 
@@ -18,7 +20,7 @@ object ReadUtils {
   val sc = spark.sparkContext
   import spark.implicits._
 
-  def readMongo(collection: String) = {
+  def readMongo(collection: String) : MongoRDD[Document] = {
     val readConfig = ReadConfig(Map("uri" -> uri, "database" -> db, "collection" -> collection))
     val load = MongoSpark.load(sc, readConfig)
     // load.show()
