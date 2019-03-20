c31bad00560a3af89a065de04846c0fbf24b1e4a
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-20 14:09:03 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ReadUtils.scala
+++ src/main/scala/com/muhalfian/spark/util/ReadUtils.scala
@@ -18,7 +18,7 @@ object ReadUtils {
   val sc = spark.sparkContext
   import spark.implicits._
 
-  def readMongo(collection: String) = {
+  def readMongo(collection: String) : MongoRDD[Document] = {
     val readConfig = ReadConfig(Map("uri" -> uri, "database" -> db, "collection" -> collection))
     val load = MongoSpark.load(sc, readConfig)
     // load.show()
