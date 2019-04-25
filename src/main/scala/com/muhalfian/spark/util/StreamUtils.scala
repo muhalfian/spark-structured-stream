f911abd2f9772de593c8e030cda40a92378d7dbf
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-04-25 08:23:49 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/StreamUtils.scala
+++ src/main/scala/com/muhalfian/spark/util/StreamUtils.scala
@@ -24,10 +24,10 @@ class StreamUtils extends Serializable {
       .set("spark.app.id", "StreamProtocolCountToMongo")
       .set("spark.mongodb.input.uri", uri)
       .set("spark.mongodb.output.uri", uri)
-      // 1 workers
-      .set("spark.executor.instances", "1")
-      // 1 cores on each workers
-      .set("spark.executor.cores", "1");
+      // // 1 workers
+      // .set("spark.executor.instances", "1")
+      // // 1 cores on each workers
+      // .set("spark.executor.cores", "1");
 
     val session = SparkSession.builder()
       .config(conf)
@@ -49,7 +49,7 @@ class StreamUtils extends Serializable {
       .set("spark.mongodb.input.uri", uri)
       .set("spark.mongodb.output.uri", uri)
       // 1 workers
-      .set("spark.executor.instances", "1")
+      // .set("spark.executor.instances", "1")
 
     val session = SparkSession.builder()
       .config(conf)
