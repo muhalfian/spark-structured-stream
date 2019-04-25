3822818bfda6985bfeb4caf53f0f8a31e186e18b
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-04-25 12:23:42 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/jobs/OnlineStream.scala
+++ src/main/scala/com/muhalfian/spark/jobs/OnlineStream.scala
@@ -82,9 +82,9 @@ object OnlineStream extends StreamUtils {
     // ======================== AGGREGATION ================================
 
     val customDF = ngramDF
-      .withColumn("text_preprocess", TextTools.merge(col("text_stemmed"), col("text_ngram_2"))).cache()
-      .select("link", "source", "description", "image", "publish_date", "title", "text", "text_html", "text_preprocess").cache()
-      .withColumn("text_selected", TextTools.select(col("text_preprocess"))).cache()
+      .withColumn("text_preprocess", TextTools.merge(col("text_stemmed"), col("text_ngram_2")))
+      .select("link", "source", "description", "image", "publish_date", "title", "text", "text_html", "text_preprocess")
+      .withColumn("text_selected", TextTools.select(col("text_preprocess")))
       .withColumn("text_aggregate", AggTools.aggregateMongo(col("text_selected"))).cache()
       // .withColumn("new_cluster", ClusterTools.onlineClustering(col("text_aggregate")))
       // .withColumn("to_centroid", ClusterTools.updateRadius(col("text_aggregate"),col("new_cluster")))
