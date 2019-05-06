a6c28bba62d234b4a4ed8affbf84c8a07ed72cc8
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-05-06 10:27:22 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/PropertiesLoader.scala
+++ src/main/scala/com/muhalfian/spark/util/PropertiesLoader.scala
@@ -12,10 +12,10 @@ object PropertiesLoader {
   val mongoUrl : String = conf.getString("MONGO_URL")
   val mongoDb : String = conf.getString("MONGO_DB")
 
-  val dbDataInit = "data_init_7"
-  val dbMasterWord = "master_word_7"
-  val dbMasterData = "master_data_7"
-  val dbMasterDataUpdate = "master_data_7"
-  val dbMasterCluster = "master_cluster_backup_7"
-  val dbMasterClusterUpdate = "master_cluster_7"
+  val dbDataInit = "data_init"
+  val dbMasterWord = "master_word"
+  val dbMasterData = "master_data"
+  val dbMasterDataUpdate = "master_data"
+  val dbMasterCluster = "master_cluster_backup"
+  val dbMasterClusterUpdate = "master_cluster"
 }
