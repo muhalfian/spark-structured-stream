ae4c03d02fc82cf6bbcc1710f6dfaccd91089d42
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-06-15 13:15:23 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ClusterTools.scala
@@ -311,9 +311,12 @@ object ClusterTools {
 
       distanceSeq = distanceSeq :+ Document.parse(s"{link : '$link', text_aggregate : '$data._2', cluster : '$clusterSelected', to_centroid: $to_centroid, datetime : $datetime}")
       var index = distanceArr.indexWhere(_._1 == link)
-      println(link)
-      println(index)
-      distanceArr(index) = (link, data._2, clusterSelected, to_centroid, datetime)
+      if(index != -1){
+          distanceArr(index) = (link, data._2, clusterSelected, to_centroid, datetime)
+      } else {
+          distanceArr += ((link, data._2, clusterSelected, to_centroid, datetime))
+      }
+
     })
 
     val masterDistance = sc.parallelize(distanceSeq)
