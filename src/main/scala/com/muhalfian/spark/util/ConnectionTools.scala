46baedf039d452c7e13fb1692a54bf10e11dc9a7
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-06-15 16:39:38 +0700

    general : debugging

--- src/main/scala/com/muhalfian/spark/util/ConnectionTools.scala
+++ src/main/scala/com/muhalfian/spark/util/ConnectionTools.scala
@@ -20,6 +20,7 @@ object ConnectionTools {
   val masterClusterDb: MongoCollection[Document] = prayugaDb.getCollection(PropertiesLoader.dbMasterCluster)
 
   def updateCentroidCluster(clusterSelected: String, newCentroid: Seq[String], to_ground: Double, angle_ground: Double, datetime: Long, newSize: Integer, newRadius: Double, link: String) = {
-      masterClusterDb.updateOne(equal("cluster", clusterSelected), set("to_ground", to_ground)).printHeadResult("Update Result: ")
+      val hasil = masterClusterDb.updateOne(equal("cluster", clusterSelected), set("to_ground", to_ground))
+      println(hasil)
   }
 }
